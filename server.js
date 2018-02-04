47bd71a97a37dd557b0f5056c40a63aa57f7c037
Shivendra <Shivendrasrivastava19@gmail.com>
2018-02-04 19:32:14 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log = `AT: ${now}, METHOD: ${req.method}, URL: ${req.url}\n`;
+  log = log.replace('GMT+0530 (India Standard Time)','');
+  console.log(log);
+fs.appendFileSync('serverlog.txt',log,(err)=>{
+  console.log(err)
+});
+next();
+});
+// app.use((req,res,next)=>{
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getYear',()=>{
+  return new Date().getFullYear();
+});
+hbs.registerHelper('boldIt',(txt)=>{
+  return txt.toUpperCase();
+});
+app.get('/',(req,res)=>{
+  res.render('home.hbs'
+  ,{
+    welcomeMessage:'Welcome,Nice to see you!',
+    title: 'Home',
+    pageName: 'Home Page',
+
+  });
+});
+
+app.get('/about',(req,res)=>{
+  // res.send("<h1>About Page</h1>")
+  res.render('about.hbs'
+  ,{
+    title: 'About',
+    pageName: 'About page',
+  });
+});
+
+
+app.get('/bad',(req,res)=>{
+  res.send({
+    errorMsg: 'Unable to process'
+  })
+});
+
+
+app.listen(3000,()=>{
+  console.log("Listening to 3000 ")
+});
